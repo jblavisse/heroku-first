b8f6742e99eab8c495a1fa8a5a770d8b740896fc
jblavisse <jean-baptiste@jblavisse.fr>
2021-06-11 11:38:07 +0200

    Init sequelize with one model

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 const express = require('express')
+// require est intelligent
+// 1. Pas besoin de .js
+// 2. Si on lui donne un dossier en chemin, il va chercher automatiquement
+// un fichier index
+// const sequelize = require("./models/index.js")
+const sequelize = require("./models");
 
 const app = express()
 
@@ -9,6 +15,16 @@ app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/services', (req, res) => res.send('Mes services'))
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+sequelize.authenticate()
+// Si c'est bon
+.then(() => {
+    app.listen(port, () => {
+        console.log(`Example app listening at http://localhost:${port}`)
+    })
+})
+// Si c'est pas bon
+.catch(err => {
+ console.log('Unable to connect to the database:');
+ console.log(err.message);
+ process.exit();
 })
---
+++ models/index.js
@@ -0,0 +1,46 @@
+// Le fichier index.js dans le dossier models
+// va servir à l'initialisation de l'ORM
+// + à la liaison avec des modèles qui sont dans d'autres fichiers
+
+// Récupérer le paquet de sequelize dans npm
+const {DataTypes, Sequelize} = require('sequelize');
+
+const dotenv = require('dotenv');
+
+// Ca va envoyer dans la variable process.env, tous les paramètres
+// dans le fichier .env
+dotenv.config();
+
+// Créer une instance de sequelize avec les paramètres de la BDD
+const sequelize = new Sequelize(
+    process.env.DB_URL
+);
+
+
+const Customer = sequelize.define('Customer', {
+    firstname: {
+        type: DataTypes.STRING(30),
+        allowNull: false
+    },
+    lastname: {
+        type: DataTypes.STRING(30),
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING(100),
+        allowNull: false
+    },
+    address: {
+        type: DataTypes.STRING(255),
+        allowNull: true
+    },
+    phone: {
+        type: DataTypes.STRING(14),
+        allowNull: true
+    }
+}, {
+        underscored: true,
+        tableName: 'customer'
+})
+
+module.exports = sequelize;
