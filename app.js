5ea5b200fc9895027b88798df82e8a2d7f879294
jblavisse <jean-baptiste@jblavisse.fr>
2021-06-11 10:59:33 +0200

    Prepare the app for heroku

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express')
 
 const app = express()
 
-const port = 3000
+// Récupérer le port d'Heroku, et sinon c'est 3000
+const port =  process.env.PORT || 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
